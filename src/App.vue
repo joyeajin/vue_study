dcff0101d3c28bed56ee5e3e988588a92c28c50a
joyeajin <dpwls31200@gmail.com>
2024-05-16 15:48:30 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div class="menu">
+    <a v-for="(menu, i) in menus" :key="i">{{menu}}</a>
+  </div>
+
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>90 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      menus : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu{
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 15px
+}
+
+.menu a{
+  color: white;
+  padding: 10px
+}
+</style>
