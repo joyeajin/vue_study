1162e67bf867c3a519f787c0edb1983c3d40a631
joyeajin <dpwls31200@gmail.com>
2024-05-16 16:48:28 +0900

    실제 데이터 import 해서 뿌리기 완료

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,58 @@
 <template>
 
+  <div class="black-bg" v-if="openModal == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="openModal = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{menu}}</a>
   </div>
 
-  <div>
-    <h4>{{products[0]}}</h4>
-    <p>50 만원</p>
+  <div v-for="(oneroom, i) in onerooms" :key='i'>
+    <img :src="onerooms[i].image" alt="" class="room-img">
+    <h4 @click="openModal = true">{{oneroom.title}}</h4>
+    <p>{{oneroom.price}}원</p>
   </div>
+
+<!-- 
   <div>
+    <img src="./assets/room1.jpg" alt="" class="room-img">
     <h4>{{products[1]}}</h4>
     <p>70 만원</p>
+    <button @click="신고수[1]++">허위매물신고</button>
+    <span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" alt="" class="room-img">
     <h4>{{products[2]}}</h4>
     <p>90 만원</p>
-  </div>
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span>신고수 : {{신고수[2]}}</span>
+  </div> -->
 </template>
 
 <script>
+
+import data from './assets/oneroom.js'
 
 export default {
   name: 'App',
   data(){
     return{
+      onerooms : data,
+      openModal : false,
+      신고수 : [0,0,0],
       menus : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods : {
+    increase(){
+      this.신고수++;
     }
   },
   components: {
@@ -34,6 +61,30 @@ export default {
 </script>
 
 <style>
+
+body{
+  margin: 0
+}
+
+div{
+  box-sizing: border-box
+}
+
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -51,5 +102,10 @@ export default {
 .menu a{
   color: white;
   padding: 10px
+}
+
+.room-img{
+  width: 100%;
+  margin-top: 40px
 }
 </style>
